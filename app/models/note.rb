83fa9f2ecde6f793ff3b10eeef421ec98b828f41
brianmenard <useyourbrian@gmail.com>
2016-04-05 19:52:41 +0000

    Done.

---
+++ app/models/note.rb
@@ -0,0 +1,17 @@
+class Note < ActiveRecord::Base
+
+  belongs_to :user
+  has_many :viewers
+  has_many :readers, through: :viewers, source: :user
+
+  def visible_to
+    readers.map { |u| u.name }.join(', ')
+  end
+
+  def visible_to=(new_readers)
+    self.readers = new_readers.split(',').map do |name|
+      User.find_by(name: name.strip)
+    end.compact
+  end
+
+end
