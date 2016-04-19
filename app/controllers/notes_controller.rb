2fec7c02c8174732ebaafb6763edcc481be2ac57
Smulligan85 <sean.mulligan85@gmail.com>
2016-04-18 22:03:33 -0700

    Done

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,12 +1,13 @@
 class NotesController < ApplicationController
-  load_and_authorize_resource
+  load_and_authorize_resource only: [:edit, :update, :show]
   def new
     @note = Note.new
   end
 
   def create
     @note = Note.new(note_params)
-    if @note.save
+    @note.user = current_user
+    if @note.save!
       redirect_to '/'
     else
       render :new
@@ -23,13 +24,8 @@ class NotesController < ApplicationController
   end
 
   def update
-    @note = Note.find(params[:id])
-    if @note.update(note_params)
-      redirect_to '/'
-    else
-      render :edit
-    end
-
+    @note.update(note_params)
+    redirect_to '/'
   end
 
   def destroy
@@ -39,6 +35,6 @@ class NotesController < ApplicationController
   private
 
   def note_params
-    params.require(:note).permit(:content)
+    params.require(:note).permit(:content, :visible_to)
   end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,14 +1,24 @@
-require 'pry'
 class Note < ActiveRecord::Base
   belongs_to :user
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
 
+  before_save :user_can_read
+
   def visible_to=(names)
-    @names = names
+    names.split(", ").each do |name| 
+      self.readers << User.find_or_create_by(name: name)
+    end
+    self.readers
   end
 
   def visible_to
-    @names
+    self.readers.collect { |a| a.name }.join(", ")
+  end
+
+  def user_can_read
+    if user && !readers.include?(user)
+      readers << user
+    end
   end
 end
