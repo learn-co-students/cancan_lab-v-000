9c2d78bcc7ed6fc5b4055460fa97ad41cd81acd7
cernanb <cernanb@gmail.com>
2016-04-24 13:57:10 -0700

    modeled permissions in the database using cancancan

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,49 @@
+class NotesController < ApplicationController
+  load_and_authorize_resource only: [:edit, :show, :update]
+  before_action :set_note, only: [:edit, :show, :destroy, :update]
+
+  def new
+    if logged_in?
+      @note = Note.new
+    else
+      redirect_to login_path
+    end
+  end
+
+  def create
+    note = Note.new(note_params)
+    note.user = current_user
+    note.save!
+    redirect_to '/'
+  end
+
+  def edit
+  end
+
+  def update
+    @note.update(note_params)
+    redirect_to '/'
+  end
+
+  def index
+    @notes = Note.none
+    if current_user
+      @notes = current_user.readable
+    end
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  private
+    def set_note
+      @note = Note.find_by(id: params[:id])
+    end
+
+    def note_params
+      params.require(:note).permit(:content, :visible_to)
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def create
+    # raise params.inspect
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/notes'
+    else
+      render 'sessions/signup'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name)
+    end
+end
