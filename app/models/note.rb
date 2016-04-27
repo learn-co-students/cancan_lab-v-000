10ee87deade443d7e92a2d40de9a62da3eb9aab5
Enoch Griffith <enoch2k2@hotmail.com>
2016-04-26 19:02:12 -0500

    Done.

---
+++ app/models/note.rb
@@ -0,0 +1,23 @@
+class Note < ActiveRecord::Base
+  belongs_to :user
+  has_many :viewers
+  has_many :readers, through: :viewers, source: :user
+
+  before_save :owner_can_read
+
+  def visible_to=(readers)
+    if readers
+      self.readers = readers.split(', ').collect{|name| User.find_by(name: name)}
+    end
+  end
+
+  def visible_to
+    readers.collect{|reader| reader.name}.join(', ')
+  end
+
+  def owner_can_read
+    if user && !readers.include?(user)
+      readers << user
+    end
+  end
+end
