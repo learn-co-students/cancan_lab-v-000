2643c4f4ac4e60933c6cc8ba859ef7adfcf127b7
rocketNeck <homestarflyer@gmail.com>
2016-04-29 22:59:54 -0400

    slight improvements

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,36 +1,38 @@
 class NotesController < ApplicationController
+  load_and_authorize_resource only: [:edit, :show, :update]
+
   def new
+    render partial: 'form', locals: {note: Note.new}
   end
 
   def create
-    if current_user
-      @note = Note.create(note_params)
-      @note.readers.push(current_user)
-      @note.user = current_user
-    end
-    redirect_to root_path
+    note = Note.new(note_params)
+    note.user = current_user
+    note.save!
+    redirect_to '/'
   end
 
-  def index
+  def update
+    @note.update(note_params)
+    redirect_to '/'
   end
 
   def edit
   end
 
-  def update
-    @note = Note.find(params[:id])
-    if @note.update(note_params)
-      redirect_to root_path
-    end
+  def show
   end
 
-
-  def destroy
+  def index
+    @notes = Note.none
+    if current_user
+      @notes = current_user.readable
+    end
   end
 
   private
 
   def note_params
-    params.require(:note).permit([:content, :visible_to])
+    params.require(:note).permit(:content, :visible_to)
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
+    return unless user
+
     can :manage, Note, {user_id: user.id}
+
     can :read, Note do |note|
-      note.readers.include?(user)
+      note.readers.include? user
     end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
