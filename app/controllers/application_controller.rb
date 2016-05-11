55d8ea3e370f38d900332100150f8f51b472f4cd
gormanjp <jpgorman8@gmail.com>
2016-05-11 17:16:08 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :current_user 
+
+  def current_user 
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id] 
+  end
+
+  def require_user 
+    redirect_to '/' unless current_user 
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => 'access denied'
+  end
+
+
+
 end
