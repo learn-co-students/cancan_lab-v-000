0eccb52052fc4a8c29fa85138f5218a4c4218d2d
Ziv Zamechek <ziv921@yahoo.com>
2016-05-16 12:32:31 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/notes'
+    else
+      render 'sessions/signup'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
