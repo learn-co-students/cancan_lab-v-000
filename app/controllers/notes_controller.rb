33b3af0c2c5e132cd85bef64d5bf046717dbc768
mirandalily <mirandaraymond@me.com>
2016-05-24 16:22:53 -0700

    Done.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,40 @@
+class NotesController < ApplicationController
+
+  def index
+  end
+
+  def create
+    if session[:user_id]
+      user = User.find(session[:user_id])
+      user.notes.build(note_params)
+      user.save
+      redirect_to '/'
+    else
+      redirect_to '/'
+    end
+  end
+
+  def update
+    if session[:user_id]
+      user = User.find(session[:user_id])
+      note = Note.find(params[:id])
+      note.update(note_params)
+    end
+    redirect_to '/'
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content, :visible_to)
+  end
+end
