2e33d7848b174223d316813b3a028eef84158d65
Heber Sandoval <he3ers@gmail.com>
2016-06-08 21:18:10 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create, :destroy]
+  resources :notes
+  resources :users, only: [:create]
+  # root "notes#index"
+  # get "user/create"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
