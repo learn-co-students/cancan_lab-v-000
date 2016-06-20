555540021ba30642243a6a6dfd16cb2df7638a97
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-06-19 22:57:20 -0400

    Done.

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,7 @@ class Note < ActiveRecord::Base
   belongs_to :user
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
+  before_create :validate_owner
 
   def visible_to
     readers.collect {|reader| reader.name}.join(', ')
@@ -9,5 +10,16 @@ class Note < ActiveRecord::Base
 
   def visible_to=(users)
     readers << users.split(', ').map {|user| User.find_by(name: user) }
+    readers.sort
   end
-end
+
+  private
+
+  def validate_owner
+    # binding.pry
+    if !readers.include?(user)
+      readers << user
+    end
+    # readers.sort_by &:id
+  end
+end 
