8b6ab7afcc9545b326d6ef46609aef1c1b64f2e7
theRingleman <sam.ringleman@gmail.com>
2016-07-11 10:56:10 -0600

    Done.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,62 @@
+class NotesController < ApplicationController
+
+  before_action :require_login
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find_by_id(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    @note.user = current_user
+    @note.readers << current_user
+    if @note.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @note = Note.find_by_id(params[:id])
+  end
+
+  def update
+    @note = Note.find_by_id(params[:id])
+    @note.update(note_params)
+    @note.readers << current_user
+     if @note.save
+      redirect_to root_path
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to root_path
+    end
+  end
+
+  def note_params
+    params.require(:note).permit(:content, :user_id, :visible_to)
+  end
+
+
+end
---
+++ app/models/note.rb
@@ -0,0 +1,15 @@
+class Note < ActiveRecord::Base
+  belongs_to :user
+  has_many :viewers
+  has_many :readers, through: :viewers, source: :user
+
+  def visible_to
+    readers.collect{|reader| reader.name}.join(", ")
+  end
+
+  def visible_to=(new_peeps)
+    self.readers = new_peeps.split(",").map do |name|
+      User.find_by(name: name.strip)
+    end.compact
+  end
+end
