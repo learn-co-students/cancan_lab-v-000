f9393d7d3964fbcea0f000114e7aa45bb2e35c42
breiss <brad72287@gmail.com>
2016-07-14 20:52:58 +0000

    Done.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,20 @@
 class NotesController < ApplicationController
+  load_and_authorize_resource
+  
+  def create
+    @note = Note.create(note_params)
+    @note.readers << current_user if !current_user.nil?
+    redirect_to "/"
+  end
+  
+  def update
+    @note.update(note_params)
+    redirect_to "/"
+  end
+  
+  def note_params
+    params.require(:note).permit(:user_id, :content, :visible_to)
+  end
+  
+
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,13 @@ class Note < ActiveRecord::Base
   belongs_to :user
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
+  
+  
+  def visible_to=(list)
+    list.split(",").each {|reader| self.readers << User.find_or_create_by(name: reader)}
+  end
+  
+  def visible_to
+    self.readers
+  end
 end
