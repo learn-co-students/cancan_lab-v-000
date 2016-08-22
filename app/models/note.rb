d8d9146717ebbf8f8b58b771645133b6dc478d15
kaileeagray <kaileeagray@gmail.com>
2016-08-22 15:29:13 -0500

    Done.

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,13 +4,24 @@ class Note < ActiveRecord::Base
   belongs_to :user
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
+  before_save :ensure_owner_can_read
 
   def visible_to=(names)
-    binding.pry
-    self.readers = names.collect do |name|
+    self.readers = names.split(',').map do |name|
+      User.find_by(name: name.strip)
     end
   end
 
   def visible_to
+    self.readers.map { |user| user.name  }.join(", ")
   end
+
+  private
+
+  def ensure_owner_can_read
+    if user && !readers.include?(user)
+      readers << user
+    end
+  end
+  
 end
