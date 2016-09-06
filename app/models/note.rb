47f0959d216bcc51a07ffd04bd7cd45322210c7a
jchu4483 <jchu4483@gmail.com>
2016-09-06 16:09:29 -0400

    Done.

---
+++ app/models/note.rb
@@ -0,0 +1,25 @@
+class Note < ActiveRecord::Base
+  belongs_to :user
+  has_many :viewers
+  has_many :readers, through: :viewers, source: :user
+
+  before_save :ensure_author_can_read_own_note
+
+  def visible_to=(users)
+    self.readers = users.gsub(" ","").split(",").map do |user|
+      User.find_by(name: user)
+    end
+  end
+
+  def visible_to
+    readers.map {|reader| reader.name}.join(", ")
+  end
+
+  private
+
+  def ensure_author_can_read_own_note
+    if user && !readers.include?(user)
+      readers << user
+    end
+  end
+end
