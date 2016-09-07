e7c6577f57462d5e9ca5bba4aa4e5deb34a93112
akizzles <aykim002@gmail.com>
2016-09-07 01:35:00 -0400

    Done.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,50 @@
+class NotesController < ApplicationController
+  load_and_authorize_resource
+  skip_authorize_resource :only => :home
+
+  def home
+
+  end
+
+  def new
+   if current_user
+     current_user.note.build
+   else
+     redirect_to root_path
+   end
+  end
+
+  def create
+   note = Note.new(note_params)
+   note.user = current_user
+   note.save
+   redirect_to root_path
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def edit
+   @note = Note.find(id: params[:id])
+  end
+
+  def update
+   note = Note.last
+   note.update(note_params)
+   note.save
+   redirect_to root_path
+  end
+
+  def destroy
+
+  end
+
+  def note_params
+   params.require(:note).permit(:name, :content, :visible_to)
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+
+  def new
+    session[:user_id] = params[:user_id]
+  end
+
+  def create
+    user = User.find(id: params[:user_id])
+    session[:user_id] = user.id
+  end
+
+  def destroy
+    session[:user_id].clear
+  end
+  
+end
