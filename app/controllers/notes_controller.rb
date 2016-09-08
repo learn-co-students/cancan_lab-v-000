a313e828d7c69c9ee3b1b187eaf8704b996e30d5
Jesse Novotny <jessenovotny@gmail.com>
2016-09-08 07:58:22 -0700

    Done.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,17 @@
 class NotesController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+    @note = Note.new
+  end
+
   def create
     note = Note.create(note_params)
+    user = User.find(session[:user_id])
+    note.readers << user
+    note.user = user
+    note.save
+    
     redirect_to root_path
   end
 
