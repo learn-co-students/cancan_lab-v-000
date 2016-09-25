e7b10f59e0616bf3fec68df1d3285fbd620c875f
CodeByLine <yleventhal@ymail.com>
2016-09-25 15:29:34 -0400

    added models controllers and edited migration tables

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def create
+    #This is the only route needed
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only => [:create]
+  resources :notes
+
+  get '/login' => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
