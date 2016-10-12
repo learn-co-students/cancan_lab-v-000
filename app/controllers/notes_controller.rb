e6177d586f9589a42dab78a36063db04b5a3718e
Kathleen <kayjsmyth@gmail.com>
2016-10-12 18:22:02 -0400

    Done.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -9,6 +9,8 @@ class NotesController < ApplicationController
 
     def create
     note = Note.new(note_params)
+    note.user = current_user
+    note.readers << current_user
     note.save!
     redirect_to '/'
   end
