844197349c799f4a2001c59709d37fbbe769fde5
Joel <joelbitar1986@gmail.com>
2016-12-13 17:18:26 +0100

    Done.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'pry'
 gem 'cancancan'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.2'
@@ -31,17 +32,16 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'web-console', group: :development
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'rspec-rails', '~> 3.0'
   gem 'capybara'
 end
-
---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,48 @@
+class NotesController < ApplicationController
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+  end
+
+  # GET /notes/new
+  def new
+    @note = Note.new
+  end
+
+  # GET /notes/1/edit
+  def edit
+  end
+
+  # POST /notes
+  # POST /notes.json
+  def create
+    @note = Note.new(note_params)
+    @note.user = current_user
+    @note.save!
+    redirect_to root_path
+  end
+
+  def update
+    @note.update(note_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @note.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def note_params
+      params.require(:note).permit(:content, :visible_to)
+    end
+end
