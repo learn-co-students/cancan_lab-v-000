47b255757dec478b85b31f4fed89d74fb64ae7da
David Kennell <davidkennell0@gmail.com>
2017-02-20 15:06:07 -0500

    Done.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+  	can :read, Note do |note|
+  		note.readers.include?(user) || note.user == user
+  	end
+  	can :update, Note do |note|
+  		note.user == user
+  	end
   end
 
 end
--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe NotesController, type: :controller do
       assert_redirected_to '/'
       note = Note.find(note_id)
       assert note.content == new_content
-      assert note.readers == [alice, beth]
+      expect(note.readers).to match_array([alice, beth])
     end
   end
 end
