414874f2783c45212925bcdd34b0aa4def1884b1
kkterai <kkterai@gmail.com>
2017-03-03 15:28:45 -0800

    Done.

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+
+    if user
+      can :manage, Note, user_id: user.id
+
+      can :read, Note do |note|
+        note.readers.include? user
+      end
+    end
+
+  end
+end
