5238d6814e22db7c120fa9094c5226397baed100
Jon Fougner <jon.fougner@gmail.com>
2017-03-03 17:54:19 +0000

    Done.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,40 @@
 class NotesController < ApplicationController
 
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+  before_action :require_login
+  skip_before_action :require_login, only: [:index]
+
+  def new
+    @note = Note.new
+  end
+
+  def show
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to @note, notice: 'Note was successfully created.'}
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:content)
+  end
+
+  def require_login
+    redirect_to '/' unless session.include? :user_id
+  end
+
 end
