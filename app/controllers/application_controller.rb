bfecafd8a2bcfb299851cbe2714a3a8c97c68efd
John Pfingst <jpfingst@outlook.com>
2017-04-14 00:32:54 +0000

    test 1 passed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to '/', :alert => exception.message
+  end
+
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session.exists? :user_id
+    @current_user ||= User.find_by(id: session[:user_id]) if session.key? :user_id
   end
 
   def logged_in?
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,5 @@
 class Note < ActiveRecord::Base
+  belongs_to :user
   has_many :viewers
   has_many :readers, through: :viewers, source: :user
 
@@ -6,7 +7,6 @@ class Note < ActiveRecord::Base
   end
 
   def visible_to=(user_list)
-    binding.pry
   end
 
 end
