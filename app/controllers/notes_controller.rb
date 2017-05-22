47ac6b2feeeb1464a4fe7c4f58bcf66e56bbea8e
brownielox <mrsmpython@gmail.com>
2017-05-22 15:06:39 -0400

    Done.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,23 @@
+class NotesController < ApplicationController
+  load_and_authorize_resource
+
+  def new
+  end
+
+  def create
+    current_user.notes.create(note_params) if logged_in?
+    redirect_to '/'
+  end
+
+  def update
+    Note.find(params[:id]).update(note_params)
+    redirect_to '/'
+  end
+
+  private
+
+    def note_params
+      params.require(:note).permit(:content, :visible_to)
+    end
+
+end
