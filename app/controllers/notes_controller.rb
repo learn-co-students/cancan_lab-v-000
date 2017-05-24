96ea89238b22f01179d10cdaba4531a31c09c443
umohm1 <umohm12@gmail.com>
2017-05-24 17:43:30 -0400

    Done.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,37 @@
+class NotesController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    @note.user = current_user
+    @note.save!
+    redirect_to '/'
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note.update(note_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:content, :visible_to)
+  end
+end
