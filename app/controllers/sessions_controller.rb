5f4b5556db0559c410f1a76cfbf3f6fb740ad454
Jon <jon_p_stone@yahoo.com>
2017-06-04 05:22:23 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  
+  def new
+    redirect_to notes_index unless !current_user
+  end
+
+  def create
+    return head(:unprocessable) unless params[:name]
+    session[:user_id] = User.find_or_create_by(name: params[:name]).id
+    redirect_to '/'
+  end
+
+  def destroy
+    session.clear
+    redirect_to '/'
+  end
+end
