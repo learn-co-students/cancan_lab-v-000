7842fb22d1ec7074a3d16746bf3273bb2da6b6bb
Scott Atkins <satkins16@gmail.com>
2017-09-06 14:01:39 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
     session[:user_id] = User.find_or_create_by(name: params[:name]).id
     redirect_to :back
   end
+
+  def destroy
+    session.delete :user_id
+    redirect_to :back
+  end
 end
