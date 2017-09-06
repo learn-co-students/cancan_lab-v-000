81dd10c58f8f83d058f69d0190f5d9d24ba942c5
Scott Atkins <satkins16@gmail.com>
2017-09-06 13:56:16 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,7 @@
+class SessionsController < ApplicationController
+  def create
+    return head(:unprocessable) unless params[:name]
+    session[:user_id] = User.find_or_create_by(name: params[:name]).id
+    redirect_to :back
+  end
+end
